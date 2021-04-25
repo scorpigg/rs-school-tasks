c91fd9c21637930179af1244ee725141d968c6ef
Andrey Nikitin <scorpigg@yandex.ru>
2021-04-25 12:05:31 +0400

    feat: add pop-ups for desktop resolution

--- online-zoo/js/main.js
+++ online-zoo/js/main.js
@@ -1,22 +1,15 @@
 window.addEventListener('load', () => {
-    // const ourPetsCards = document.querySelectorAll('.pet');
-
-    // ourPetsCards.forEach(el => { 
-    //     el.addEventListener('click', (e) => {
-    //         const pet = el.dataset.pet;
-    //         if (pet) {
-    //             document.location.href = `/zoos/${pet}.html`;
-    //         }
-    //     })
-    // });
-
-    const donationAmount = document.querySelector('.quick-donation__value');
-    donationAmount.addEventListener('input', () => {
-        if (donationAmount.value.length > 4) {
-            donationAmount.value = donationAmount.value.slice(0, -1);
-            donationAmount.value = 9999;
-        }
-    })
+    const donationAmount = document.querySelectorAll('.donation-amount');
+    
+    donationAmount.forEach(input => {
+        input.addEventListener('input', () => {
+            if (input.value.length > 4) {
+                input.value = input.value.slice(0, -1);
+                input.value = 9999;
+            }
+        })
+    });
+    
     
 
 })
